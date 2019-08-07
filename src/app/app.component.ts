16c1fee9407681d3c472ead29d02892e80f41346
Manjur Ahmed <a.manjur06@gmail.com>
2019-08-06 21:13:56 -0400

    Saving progress of w.e

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { Card, Section } from './app.models';
+import { AppService } from './app.service';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
-  title = 'sticky-notes-FE';
+  constructor(private appService: AppService) {
+
+  }
+  sections: Section[] = [
+    { id: 'To Do', cols: 1, rows: 2, color: 'lightblue', notesCollections: [] },
+    { id: 'In Progress', cols: 2, rows: 1, color: 'lightpink', notesCollections: [] },
+    { id: 'Done', cols: 1, rows: 2, color: 'lightgreen', notesCollections: [] },
+    { id: 'Failed', cols: 2, rows: 1, color: '#ff5b5b', notesCollections: [] },
+  ];
+
+  addNote(section: Section) {
+    const index = section.notesCollections.length;
+    section.notesCollections[index] = [];
+    section.notesCollections[index][0] = new Card(this.appService.getNewCardId(), section.id, index, 0);
+  }
+
+  removeNote(id: number) {
+    for (let sectionIndex = 0; sectionIndex < this.sections.length; sectionIndex++) {
+      const section = this.sections[sectionIndex];
+      for (let collectionIndex = 0; collectionIndex < this.sections[sectionIndex].notesCollections.length; collectionIndex++) {
+        const collection = section.notesCollections[collectionIndex];
+        const index = collection.findIndex((note) => note.noteId === id);
+        if (index >= 0) {
+          collection.splice(collection.findIndex((note) => note.noteId === id));
+          if (!collection.length) {
+            section.notesCollections.splice(collectionIndex, 1);
+          }
+          return;
+        }
+      }
+    }
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { StickyNoteComponent } from './sticky-note/sticky-note.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { AppService } from './app.service';
+import { FocusService } from './services/focus.service';
+import { FormsModule } from '@angular/forms';
+import { ContentEditbaleDirective } from './directives/editable-content.directive';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StickyNoteComponent,
+    ContentEditbaleDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatGridListModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    FormsModule,
+    DragDropModule
   ],
-  providers: [],
+  providers: [AppService, FocusService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
